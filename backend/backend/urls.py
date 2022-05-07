f67533e60976b88f630b90250a298069301fe557
Кирилл <k_kirill45@mail.ru>
2022-05-07 11:09:33 +0300

    Create views. Update urls, templates

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,5 +6,5 @@ from .views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main)
+    path('', main, name='main')
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+import datetime
 
 
 def main(request):
+    context = {}
+    if request.method == 'POST':
+        date_from_request = request.POST['date'].split('-')
+        date_start = datetime.date(2019, 1, 1)
+        date_end = datetime.date(*[int(i) for i in date_from_request])
+        weeks = (date_end - date_start).days // 7 + 1
+        if date_end.isocalendar()[2] == 1:
+            weeks += 1
+        context['weeks'] = weeks
     return render(
-        request, 'index.html', {}
+        request, 'index.html', context
     )
+
+##52, 53
+
