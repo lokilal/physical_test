4f311bdee68d0c6f1b9c892cdbcb5a87b0c288c8
Кирилл <k_kirill45@mail.ru>
2022-05-07 15:46:01 +0300

    Update views

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -12,10 +12,13 @@ def main(request):
         date_from_request = request.POST['date'].split('-')
         date_start = datetime.date(2019, 1, 1)
         date_end = datetime.date(*[int(i) for i in date_from_request])
-        weeks = (date_end - date_start).days // 7 + 1
-        if date_end.isocalendar()[2] == 1:
-            weeks += 1
-        context['weeks'] = weeks
+        if date_end < date_start:
+            context['weeks'] = 'Выберите дату после 01.01.2019'
+        else:
+            weeks = (date_end - date_start).days // 7 + 1
+            if date_end.isocalendar()[2] == 1:
+                weeks += 1
+            context['weeks'] = weeks
     return render(
         request, 'index.html', context
     )
